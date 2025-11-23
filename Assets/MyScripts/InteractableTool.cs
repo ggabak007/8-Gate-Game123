37e638a69e462a97f4f116cab3732ed85264c977
이수민 <soomin426437@gmail.com>
2025-11-24 00:01:26 +0900

    코드 폴더 수정

--- Assets/MyScripts/InteractableTool.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class InteractableTool : MonoBehaviour
-{
-    [Header("도구 설정")]
-    public ToolType toolType; // 이 도구가 무엇인지 (Inspector에서 설정)
-
-    // PlayerInventory가 이 도구를 주울 때 정보를 가져가기 위해 이 스크립트가 존재함
-}
